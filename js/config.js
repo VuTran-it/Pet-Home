6302067844d63d8105785e65d34e2ab13c67f053
VuTran-it <88641712+VuTran-it@users.noreply.github.com>
2023-03-18 23:01:14 +0700

    auto

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
-const hosting = "https://raspberry-test-0207.firebaseapp.com";
+const hosting = "http://127.0.0.1:5501"//"https://raspberry-test-0207.firebaseapp.com";
 const adminID = 'uVfRX0jsV3dl3HgZ9dAciEoCssT2';
